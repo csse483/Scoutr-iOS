c993e59a0e2a8c78ec0221b2215cd8df00dd59df
Rahul Yarlagadda <yarlagrt@rose-hulman.edu>
2016-07-20 15:35:17 -0400

    Initial commit of a single page Xcode project template

---
+++ Scoutr/Scoutr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Scoutr
+//
+//  Created by CSSE Department on 7/20/16.
+//  Copyright © 2016 Rose Hulman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
