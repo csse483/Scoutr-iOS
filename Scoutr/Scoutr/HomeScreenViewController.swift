320b96b056f3f2910b416fd4a6ac8e4cbf8254d4
Rahul Yarlagadda <yarlagrt@rose-hulman.edu>
2016-07-25 17:25:00 -0400

    Successfully picks tournament and sends key back to home screen.

--- Scoutr/Scoutr/HomeScreenViewController.swift
+++ Scoutr/Scoutr/HomeScreenViewController.swift
@@ -9,9 +9,13 @@
 import UIKit
 
 class HomeScreenViewController: UIViewController {
+    
+    var tournamentKey : String?
+    let setUpTournamentSegueIdentifier = "setUpTournamentSegue"
+    let viewDataSegueIdentifier = "viewDataSegue"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,14 +24,24 @@ class HomeScreenViewController: UIViewController {
     }
     
 
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        if (segue.identifier == setUpTournamentSegueIdentifier) {
+            //let destinationViewController = segue.destinationViewController as! SetUpTournamentTableViewController
+            NSLog("Going to set up tournament screen")
+        }
+        else if (segue.identifier == viewDataSegueIdentifier) {
+            //let destinationViewController = segue.destinationViewController as! ViewDataTableViewController
+            NSLog("Going to view data tournmanet screen")
+        }
+        
+            
     }
-    */
+    
+    @IBAction func unwindToHome(seuge: UIStoryboardSegue){}
 
 }
--- Scoutr/Scoutr/SetUpTournamentTableViewController.swift
+++ Scoutr/Scoutr/SetUpTournamentTableViewController.swift
@@ -9,15 +9,19 @@
 import UIKit
 
 class SetUpTournamentTableViewController: UITableViewController {
+    
+    var tournaments : [Tournament] = []
+    let cellIdentifier = "tournamentCell"
+    let segueIdentifier = "unwindToHome"
+    
+    struct Tournament {
+        let key : String
+        let name : String
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        addFakeData() //TODO: Remove this when JSON parsing of TBA data is implemented
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,27 +29,36 @@ class SetUpTournamentTableViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    func addFakeData() {
+        tournaments.append(Tournament(key: "2016abca", name: "Western Canada Regional"))
+        tournaments.append(Tournament(key: "2016alhu", name: "Rocket City Regional"))
+        tournaments.append(Tournament(key: "2016arc", name: "Archimedes Division"))
+        tournaments.append(Tournament(key: "2016arlr", name: "Arkansas Rock City Regional"))
+        tournaments.append(Tournament(key: "2016aroz", name: "Ozark Mountain Brawl"))
+        tournaments.append(Tournament(key: "2016ausy", name: "Australia Regional"))
+        tournaments.append(Tournament(key: "2016azfl", name: "Arizona North Regiona"))
+        
     }
+
+    // MARK: - Table view data source
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return tournaments.count
     }
 
-    /*
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath)
+        cell.textLabel!.text = tournaments[indexPath.row].name
+        cell.detailTextLabel!.text = tournaments[indexPath.row].key
         return cell
     }
-    */
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        self.performSegueWithIdentifier(segueIdentifier, sender: tournaments[indexPath.row].key)
+    }
+    
 
     /*
     // Override to support conditional editing of the table view.
@@ -82,14 +95,19 @@ class SetUpTournamentTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        if (segue.identifier == segueIdentifier) {
+            let destinationViewController = segue.destinationViewController as! HomeScreenViewController
+            destinationViewController.tournamentKey = (sender as! String)
+            NSLog("Going back to home screen. Set up for the \(sender as! String)")
+        }
     }
-    */
+ 
 
 }
